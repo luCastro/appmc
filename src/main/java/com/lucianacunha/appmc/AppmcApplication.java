e02ec3644c6c22b902c87cc9d4fe853a3d5a3edb
Luciana Cunha <lucastro.marquise@gmail.com>
2018-09-18 16:44:46 -0700

    The project was create

---
+++ src/main/java/com/lucianacunha/appmc/AppmcApplication.java
@@ -0,0 +1,12 @@
+package com.lucianacunha.appmc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppmcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppmcApplication.class, args);
+	}
+}
