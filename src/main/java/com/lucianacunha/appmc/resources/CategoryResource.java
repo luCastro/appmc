06d86aa6cabb13ad5ce080e8c80ff03688dc8e63
Luciana Cunha <lucastro.marquise@gmail.com>
2018-09-18 17:01:21 -0700

    Testing REST

---
+++ src/main/java/com/lucianacunha/appmc/resources/CategoryResource.java
@@ -0,0 +1,18 @@
+package com.lucianacunha.appmc.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/categories")
+public class CategoryResource {
+	
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public String lisAll() {
+		
+		return "First action";
+	}
+
+}
